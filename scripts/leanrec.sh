9038e79453e87afbdfa5ace350a4457b63b37a4f
Peter Bennett <pbennett@mythtv.org>
2023-01-25 17:45:29 -0500

    leanrec: Allow for TVMA and TV-14 from Peacock

--- scripts/leanrec.sh
+++ scripts/leanrec.sh
@@ -407,10 +407,10 @@ while true ; do
             # peacock prompt: Cancel on last line
             # peacock prompt: "Up Next" on a whole line
             # tubi prompt: Starting in xx secondsS
-            # peacock prompt - i TVMA
+            # peacock prompt - i TVMA or i TV-14
             # Hulu no ads prompt "Episodes Inside the Episodes"
             # HBOMAX: "AUTOPLAY OFF" or "NEXT EPISODE " or "Seasons [0-9]"
-            if egrep -a '^Cancel$|^Up Next$|^i *TV..$|^Starting in [0-9]* seconds|Episodes|AUTOPLAY OFF|NEXT EPISODE |Seasons [0-9]' $TEMPDIR/${recname}_capture_crop.txt ; then
+            if egrep -a '^Cancel$|^Up Next$|^i *TV.{2,3}$|^Starting in [0-9]* seconds|Episodes|AUTOPLAY OFF|NEXT EPISODE |Seasons [0-9]' $TEMPDIR/${recname}_capture_crop.txt ; then
                 sleep 2
                 echo `$LOGDATE` "Recording $recfile ended with text prompt."
                 break 2
